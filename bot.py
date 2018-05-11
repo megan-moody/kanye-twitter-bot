2f2115233b4441f53164251d025cff67cfe8802f
mecosu <30613825+mecosu@users.noreply.github.com>
2018-05-10 19:55:27 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
 import datetime
 from authentication import api
+import random
 
-message = "The time is {}. Do you know where your twitter bot is?".format(
-    datetime.datetime.now().strftime('%-I:%m %p'))
-print('posting this clever message to twitter:')
-print(message)
+#probably should move all this to a main function?
+last_response = api.user_timeline(screen_name = '@GospelofKanye', count=1) #returns a list of recent statuses
+most_recent_status_id = last_response[0].id
+most_recent_kanye_tweets = api.user_timeline(screen_name = '@kanyewest') #pull most recent 20 tweets from kanye's twitter feed
+for tweet in most_recent_kanye_tweets:
+    kanye_tweet_id = tweet.id
+    if kanye_tweet_id > most_recent_status_id:
+        single_tweet = tweet.text # extract tweet text only
+        if 'https' not in single_tweet: #filter out tweets of images, videos, links, etc.
+            chapter = str(random.randint(1,51)) #randomly generate a fake bible chapter number
+            verse = str(random.randint(1,26)) #randomly generate a fake verse number
+            message = ' Kanye ' + chapter + ':' + verse + ' ' + single_tweet #create a new message with the text, plus "Kanye chapter:verse"
+            print(message)
 
-api.update_status(message)
-
-print('success!')
+api.update_status(message) 
